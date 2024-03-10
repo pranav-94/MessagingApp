7bc84aa990cd5726de2c55ab4fb7370407cbaf49
Pranav <ppranavvvvv918@gmail.com>
2024-03-10 17:36:21 +0530

    Update index.js

--- Backend/index.js
+++ Backend/index.js
@@ -6,7 +6,7 @@ const cors = require('cors')
 const { userData, signUpData } = require('./db')
 const app = express()
 
-const port = process.env.PORT
+const port = process.env.PORT || 3000
 
 const key = 'deadpool'
 
